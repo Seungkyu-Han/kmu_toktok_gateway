d3239af6a1045b7a138c9702add9ebc246cae19f
Seungkyu-Han <trust1204@gmail.com>
2024-11-27 13:05:18 +0900

    FIX: cors error

--- chat/src/main/kotlin/org/seungkyu/config/SwaggerConfig.kt
+++ chat/src/main/kotlin/org/seungkyu/config/SwaggerConfig.kt
@@ -3,11 +3,16 @@ package org.seungkyu.config
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class SwaggerConfig {
+class SwaggerConfig(
+    @Value("\${swagger.url}")
+    val path: String
+) {
 
     @Bean
     fun openApi(): OpenAPI {
@@ -21,5 +26,6 @@ class SwaggerConfig {
                     version = "1.0.0"
                 }
             )
+            .addServersItem(Server().url(path).description("API for Swagger"))
     }
 }
